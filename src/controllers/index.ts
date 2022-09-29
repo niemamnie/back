b0e582d1a16d666c4c89dd78961513fb922afa69
Robert <robert.ostrowski@bredex.de>
2022-09-29 10:08:59 +0200

    short import fixes

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,4 +1,1 @@
 export * from "./game/table-tennis.controller";
-export * from './game-player-game-player.controller';
-export * from './game-player.controller';
-export * from './game-game-player.controller';
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,4 +1,4 @@
 export * from './game-player.model';
+export * from './game.model';
 export * from './player.model';
-export * from './tabletennis';
 
